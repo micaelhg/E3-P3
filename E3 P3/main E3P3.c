77d0088dd0184c50d2a5b0f8c29b0d399840ff42
Micael <hidalgomicael@gmail.com>
2015-10-11 16:19:50 -0300

    actualizacion

--- E3 P3/main E3P3.c
+++ E3 P3/main E3P3.c
@@ -2,20 +2,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <"nodo.h">
+#include "nodo.h"
+
 
 #define m 10
 
 
+typedef struct tabHash{
+     nodo *arreglo[m]; //Arreglo de punteros a tipo nodo
+}tablaHash;
+
+
 int main()
 {
-    nodo *tabla[m]; //Arreglo de punteros a tipo nodo
-    FILE *datos = fopen("PDPE.csv","r");
-    if (datos!=NULL){
-
+    //abriendo el archivo
+    FILE *ptr = fopen("import.csv","r");
+    if (ptr!=NULL){
+        printf("Se ha abierto el archivo");
     }else{
         printf("Error al abrir archivo! \n");
     }
-
+    //Leyendo
+    char caracter=fgetc(ptr);
+    char consumir;
+    while (caracter != ','){
+        caracter=fgetc(ptr);
+        if (caracter =='"'){
+            consumir=getc(ptr);
+        }
+        printf("%c  ", caracter);
+   }
     return 0;
 }
--- E3 P3/nodo.h
+++ E3 P3/nodo.h
@@ -1,8 +1,23 @@
+#ifndef stdio_h
+#include <stdio.h>
+#define stdio_h
+#endif
+
+#ifndef stdlib_h
+#include <stdlib.h>
+#define stdio_h
+#endif
+
 
 typedef long int key;
 
 typedef struct TipoRegistro{
-  int rut;
+  int periodo;
+  int mes;
+  char *paisOrigen[15];
+  int arancel;
+  int totalCifItemUS;
+  int CantidadMercancia;
 }registro;
 
 typedef struct TipoNodo{
